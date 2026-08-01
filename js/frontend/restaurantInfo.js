74c8ce715559844f96aeb097ab9cf65163045a32
Hill <hilary.hjr@gmail.com>
2026-08-01 14:13:31 +0200

    maybe
    Replaced hardcoded restaurant data with a dynamic array and added a function to add new restaurants.

--- js/frontend/restaurantInfo.js
+++ js/frontend/restaurantInfo.js
@@ -1,35 +1,18 @@
 // To be used to store the information gathered about restaurants
 // I created these 3 just to see if the swipping animation works
 // still to do: dietary, delivery, menu link
-let RestaurantInfo = [
-    {
-        Name: "Panera Bread",
-        image: "data/panera.jpeg",
-        Cuisine: "Cafe / Bakery",
-        Hours: "8:00 AM - 9:00 PM",
-        Address: "Campus Center",
-        Distance: "1.2 miles away",
-        Rating: "4.1",
-        Price: "$$"
-    },
-    {
-        Name: "McDonald's",
-        image: "data/mcdonalds.jpeg",
-        Cuisine: "American, Fast Food",
-        Hours: "7AM - 9PM",
-        Address: "123 Main Street",
-        Distance: "1.5 miles away",
-        Rating: "3.8",
-        Price: "$"
-    },
-    {
-        Name: "Applebee's",
-        image: "data/applebee-s-neighborhood.jpeg",
-        Cuisine: "American, Casual dining",
-        Hours: "10:30 AM - 10:00 PM",
-        Address: "250 Oak Ave",
-        Distance: "2.0 miles away",
-        Rating: "4.0",
-        Price: "$$"
-    }
-];
+let RestaurantInfo = [];
+
+funtion addRestaurant(name, image, cuisine, hours, address, distance, rating, price) {
+RestaurantInfo.push({
+    Name: name,
+    image: image,
+    Cuisine: cuisine,
+    Hours: hours,
+    Address: address,
+    Distance: distance,
+    Rating: rating,
+    Price: price
+});
+}
+    
